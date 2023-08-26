c471555ba7e4e737e4ad2b74cbe8556796c2f7fa
jvyden <jvyden@jvyden.xyz>
2023-08-25 22:48:05 -0400

    Initial Commit

---
+++ DeferExpression.Example/Program.cs
@@ -0,0 +1,4 @@
+﻿using DeferExpression;
+
+using Defer _ = new Defer(() => Console.WriteLine("Defer"));
+Console.WriteLine("Hello, World!");
---
+++ DeferExpression/Defer.cs
@@ -0,0 +1,17 @@
+﻿namespace DeferExpression;
+
+public class Defer : IDisposable
+{
+    private readonly Action _action;
+    
+    public Defer(Action action)
+    {
+        this._action = action;
+    }
+
+    public void Dispose()
+    {
+        _action.Invoke();
+        GC.SuppressFinalize(this);
+    }
+}
